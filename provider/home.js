fb713ec0389258aef28a75a48e8a7bc6f14196ae
Aozaki <i@aozaki.cc>
2025-01-19 22:48:47 +0800

    chore: prettier

--- provider/home.js
+++ provider/home.js
@@ -1,16 +1,16 @@
 module.exports = {
-    type: 'custom',
-    nodeList: [
-        {
-            enable: true,
-            type: 'shadowsocks',
-            nodeName: '🏠 Home',
-            hostname: 'example.com',
-            port: 12345,
-            method: 'aes-256-gcm',
-            password: 'example.password',
-            udpRelay: true,
-            testUrl: 'http://connectivitycheck.platform.hicloud.com/generate_204',
-        },
-    ],
+  type: 'custom',
+  nodeList: [
+    {
+      enable: true,
+      type: 'shadowsocks',
+      nodeName: '🏠 Home',
+      hostname: 'example.com',
+      port: 12345,
+      method: 'aes-256-gcm',
+      password: 'example.password',
+      udpRelay: true,
+      testUrl: 'http://connectivitycheck.platform.hicloud.com/generate_204',
+    },
+  ],
 }
